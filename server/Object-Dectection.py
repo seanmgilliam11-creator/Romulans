70fb3fbe20064ac78a8223cf254f29bd5a179115
briannasnell <brisnell11@gmail.com>
2026-04-19 19:21:28 -0400

    Split up the python behaviors into their own files.

--- server/Object-Dectection.py
+++ server/Object-Dectection.py
@@ -1,11 +1,19 @@
 String color = mbot.getColorObjectFromCamera(false);
 
+System.out.println("Detected color: " + color);
+
 if (color.equalsIgnoreCase("GREEN")) {
     System.out.println("Movable object");
+    mbot.moveForward(30);
+
 } else if (color.equalsIgnoreCase("BLUE")) {
     System.out.println("Immovable object");
+    mbot.turn(45);
+
 } else if (color.equalsIgnoreCase("RED")) {
-    System.out.println("Sample object");
+    System.out.println("Sample detected");
+    mbot.stop();
+
 } else {
     System.out.println("Unknown object");
 }
